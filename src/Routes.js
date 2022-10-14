cfa30b5bac81e7fe07f31dde7452bee7e60c7429
Aniket Sharma <aniket8439@gmail.com>
2022-10-14 18:00:58 +0530

    manageproducts

--- src/Routes.js
+++ src/Routes.js
@@ -10,6 +10,7 @@ import AdminDashBoard from './user/AdminDashBoard';
 import AddCategory from './admin/AddCategory';
 import ManageCategories from './admin/ManageCategories';
 import AddProduct from './admin/AddProduct';
+import ManageProducts from './admin/ManageProducts';
 
 
 
@@ -26,6 +27,7 @@ export default function Routes() {
         <AdminRoute path = "/admin/create/category" exact component = { AddCategory }/>
         <AdminRoute path = "/admin/categories" exact component = { ManageCategories }/>
         <AdminRoute path = "/admin/create/product" exact component = { AddProduct }/>
+        <AdminRoute path = "/admin/products" exact component = { ManageProducts }/>
 
         </Switch>
 
