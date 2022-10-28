0e698c369e2538633820fb3199ad7517af220953
Aniket Sharma <aniket8439@gmail.com>
2022-10-28 18:49:07 +0530

    home and cart

--- src/Routes.js
+++ src/Routes.js
@@ -12,6 +12,7 @@ import ManageCategories from './admin/ManageCategories';
 import AddProduct from './admin/AddProduct';
 import ManageProducts from './admin/ManageProducts';
 import UpdateProduct from './admin/UpdateProduct';
+import Cart from './core/Cart';
 
 
 
@@ -23,6 +24,7 @@ export default function Routes() {
         <Route path = "/" exact component = { Home }/> 
         <Route path = "/signup" exact component = { Signup }/> 
         <Route path = "/signin" exact component = { Signin }/> 
+        <Route path = "/cart" exact component = { Cart} />
         <PrivateRoute path = "/user/dashboard" exact component = { UserDashBoard }/> 
         <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
         <AdminRoute path = "/admin/create/category" exact component = { AddCategory }/>
